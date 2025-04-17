472e64df932a7f795abeda6fdb47a4632b855f24
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-04-17 14:23:43 +0530

    delete .env folder

--- TrustWell Hospital/DBHelper.cs
+++ TrustWell Hospital/DBHelper.cs
@@ -6,14 +6,13 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using MySql.Data.MySqlClient;
-using data.env;
 
 
 namespace WindowsFormsApp1
 {
     public static class DBHelper
     {
-        private static string connectionString = "server=SERVER;database=DATABASE;user=USER;password=PASS";
+        private static string connectionString = "server=157.245.99.237;database=hospital11;user=groupAE;password=php@$aelinx_";
 
         public static MySqlConnection GetConnection()
         {
