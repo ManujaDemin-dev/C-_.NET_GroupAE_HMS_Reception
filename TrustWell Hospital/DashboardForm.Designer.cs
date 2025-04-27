7e8d0ac7a85310716954c7dad9ae4a38194d23ca
Sithija_Imesh <156312972+Sithijaimesh@users.noreply.github.com>
2025-04-27 10:34:50 +0530

    lab report
    created

--- TrustWell Hospital/DashboardForm.Designer.cs
+++ TrustWell Hospital/DashboardForm.Designer.cs
@@ -387,7 +387,11 @@
         private System.Windows.Forms.Button button2;
 <<<<<<< Updated upstream
 <<<<<<< Updated upstream
+<<<<<<< Updated upstream
         private System.Windows.Forms.Label label9;
+=======
+        private System.Windows.Forms.Button button7;
+>>>>>>> Stashed changes
 =======
         private System.Windows.Forms.Button button7;
 >>>>>>> Stashed changes
