bdd78a1ec48121663e458fe96e8fbba0fe034398
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-05-18 16:15:09 +0530

    CSproj file update :)
    schedule_Doc file update UI is not fixed

---
+++ TrustWell Hospital/Schedule_doc.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TrustWell_Hospital
+{
+    public partial class Schedule_doc: Form
+    {
+        private int DOCID;
+        public Schedule_doc(int doctorID)
+        {
+            InitializeComponent();
+            DOCID = doctorID;
+            label1.Text = $"hELLOW DOC ID : {DOCID}";
+        }
+
+        private void Schedule_doc_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
