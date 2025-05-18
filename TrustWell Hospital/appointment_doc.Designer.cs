937cfd47e21ef46b7ac3a32c084d87d42754a902
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-05-18 15:49:33 +0530

    Doctor Update : patients appointment table and data pass
    👍

--- TrustWell Hospital/appointment_doc.Designer.cs
+++ TrustWell Hospital/appointment_doc.Designer.cs
@@ -29,112 +29,166 @@
         private void InitializeComponent()
         {
             System.Windows.Forms.Label label1;
-            System.Windows.Forms.Label label2;
-            System.Windows.Forms.Label label3;
-            System.Windows.Forms.Label label4;
-            System.Windows.Forms.Label label5;
-            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            System.Windows.Forms.Label label6;
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle3 = new System.Windows.Forms.DataGridViewCellStyle();
             this.button1 = new System.Windows.Forms.Button();
+            this.gunaDataGridViewPatients = new Guna.UI.WinForms.GunaDataGridView();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             label1 = new System.Windows.Forms.Label();
-            label2 = new System.Windows.Forms.Label();
-            label3 = new System.Windows.Forms.Label();
-            label4 = new System.Windows.Forms.Label();
-            label5 = new System.Windows.Forms.Label();
+            label6 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.gunaDataGridViewPatients)).BeginInit();
             this.SuspendLayout();
             // 
             // label1
             // 
             label1.AutoSize = true;
             label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label1.Location = new System.Drawing.Point(32, 22);
+            label1.Location = new System.Drawing.Point(38, 88);
             label1.Name = "label1";
-            label1.Size = new System.Drawing.Size(250, 29);
+            label1.Size = new System.Drawing.Size(0, 29);
             label1.TabIndex = 1;
-            label1.Text = "Doctor id : {Doctor_id}";
+            label1.Click += new System.EventHandler(this.label1_Click);
             // 
-            // label2
+            // label6
             // 
-            label2.AutoSize = true;
-            label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label2.Location = new System.Drawing.Point(32, 117);
-            label2.Name = "label2";
-            label2.Size = new System.Drawing.Size(345, 29);
-            label2.TabIndex = 2;
-            label2.Text = "Specialization : {specialization}";
-            // 
-            // label3
-            // 
-            label3.AutoSize = true;
-            label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label3.Location = new System.Drawing.Point(31, 167);
-            label3.Name = "label3";
-            label3.Size = new System.Drawing.Size(425, 29);
-            label3.TabIndex = 3;
-            label3.Text = "Appointment date : {Appointment date}";
-            // 
-            // label4
-            // 
-            label4.AutoSize = true;
-            label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label4.Location = new System.Drawing.Point(31, 69);
-            label4.Name = "label4";
-            label4.Size = new System.Drawing.Size(340, 29);
-            label4.TabIndex = 4;
-            label4.Text = "Doctor Name : {Doctor_Name}";
-            // 
-            // label5
-            // 
-            label5.AutoSize = true;
-            label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            label5.ForeColor = System.Drawing.Color.Red;
-            label5.Location = new System.Drawing.Point(33, 212);
-            label5.Name = "label5";
-            label5.Size = new System.Drawing.Size(151, 20);
-            label5.TabIndex = 5;
-            label5.Text = "appointment status";
-            // 
-            // dateTimePicker1
-            // 
-            this.dateTimePicker1.Location = new System.Drawing.Point(462, 174);
-            this.dateTimePicker1.Name = "dateTimePicker1";
-            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
-            this.dateTimePicker1.TabIndex = 0;
-            this.dateTimePicker1.ValueChanged += new System.EventHandler(this.dateTimePicker1_ValueChanged);
+            label6.AutoSize = true;
+            label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            label6.Location = new System.Drawing.Point(555, 23);
+            label6.Name = "label6";
+            label6.Size = new System.Drawing.Size(204, 29);
+            label6.TabIndex = 7;
+            label6.Text = "New Appointment";
             // 
             // button1
             // 
             this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.Location = new System.Drawing.Point(37, 283);
+            this.button1.Location = new System.Drawing.Point(1085, 568);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(170, 44);
             this.button1.TabIndex = 6;
             this.button1.Text = "Confirm";
             this.button1.UseVisualStyleBackColor = true;
             // 
+            // gunaDataGridViewPatients
+            // 
+            dataGridViewCellStyle1.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridViewPatients.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle1;
+            this.gunaDataGridViewPatients.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.gunaDataGridViewPatients.BackgroundColor = System.Drawing.Color.White;
+            this.gunaDataGridViewPatients.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.gunaDataGridViewPatients.CellBorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gunaDataGridViewPatients.ColumnHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle2.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.gunaDataGridViewPatients.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle2;
+            this.gunaDataGridViewPatients.ColumnHeadersHeight = 4;
+            dataGridViewCellStyle3.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle3.BackColor = System.Drawing.Color.White;
+            dataGridViewCellStyle3.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            dataGridViewCellStyle3.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            dataGridViewCellStyle3.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            dataGridViewCellStyle3.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.gunaDataGridViewPatients.DefaultCellStyle = dataGridViewCellStyle3;
+            this.gunaDataGridViewPatients.EnableHeadersVisualStyles = false;
+            this.gunaDataGridViewPatients.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDataGridViewPatients.Location = new System.Drawing.Point(43, 239);
+            this.gunaDataGridViewPatients.Name = "gunaDataGridViewPatients";
+            this.gunaDataGridViewPatients.RowHeadersVisible = false;
+            this.gunaDataGridViewPatients.RowHeadersWidth = 51;
+            this.gunaDataGridViewPatients.RowTemplate.Height = 24;
+            this.gunaDataGridViewPatients.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
+            this.gunaDataGridViewPatients.Size = new System.Drawing.Size(1185, 260);
+            this.gunaDataGridViewPatients.TabIndex = 8;
+            this.gunaDataGridViewPatients.Theme = Guna.UI.WinForms.GunaDataGridViewPresetThemes.Guna;
+            this.gunaDataGridViewPatients.ThemeStyle.AlternatingRowsStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridViewPatients.ThemeStyle.AlternatingRowsStyle.Font = null;
+            this.gunaDataGridViewPatients.ThemeStyle.AlternatingRowsStyle.ForeColor = System.Drawing.Color.Empty;
+            this.gunaDataGridViewPatients.ThemeStyle.AlternatingRowsStyle.SelectionBackColor = System.Drawing.Color.Empty;
+            this.gunaDataGridViewPatients.ThemeStyle.AlternatingRowsStyle.SelectionForeColor = System.Drawing.Color.Empty;
+            this.gunaDataGridViewPatients.ThemeStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridViewPatients.ThemeStyle.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDataGridViewPatients.ThemeStyle.HeaderStyle.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.gunaDataGridViewPatients.ThemeStyle.HeaderStyle.BorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
+            this.gunaDataGridViewPatients.ThemeStyle.HeaderStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.gunaDataGridViewPatients.ThemeStyle.HeaderStyle.ForeColor = System.Drawing.Color.White;
+            this.gunaDataGridViewPatients.ThemeStyle.HeaderStyle.HeaightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.EnableResizing;
+            this.gunaDataGridViewPatients.ThemeStyle.HeaderStyle.Height = 4;
+            this.gunaDataGridViewPatients.ThemeStyle.ReadOnly = false;
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.BackColor = System.Drawing.Color.White;
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.BorderStyle = System.Windows.Forms.DataGridViewCellBorderStyle.SingleHorizontal;
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.Font = new System.Drawing.Font("Segoe UI", 10.5F);
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.Height = 24;
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(231)))), ((int)(((byte)(229)))), ((int)(((byte)(255)))));
+            this.gunaDataGridViewPatients.ThemeStyle.RowsStyle.SelectionForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(71)))), ((int)(((byte)(69)))), ((int)(((byte)(94)))));
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(40, 88);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(139, 25);
+            this.label7.TabIndex = 9;
+            this.label7.Text = "Doctor ID : {Id}";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(40, 127);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(214, 25);
+            this.label4.TabIndex = 10;
+            this.label4.Text = "Doctor Name : {NAME}";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(40, 167);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(214, 25);
+            this.label2.TabIndex = 11;
+            this.label2.Text = "Doctor Name : {NAME}";
+            // 
             // appointment_doc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(240)))), ((int)(((byte)(244)))), ((int)(((byte)(248)))));
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1302, 638);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.gunaDataGridViewPatients);
+            this.Controls.Add(label6);
             this.Controls.Add(this.button1);
-            this.Controls.Add(label5);
-            this.Controls.Add(label4);
-            this.Controls.Add(label3);
-            this.Controls.Add(label2);
             this.Controls.Add(label1);
-            this.Controls.Add(this.dateTimePicker1);
             this.Name = "appointment_doc";
             this.Text = "appointment_doc";
             this.Load += new System.EventHandler(this.appointment_doc_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.gunaDataGridViewPatients)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.DateTimePicker dateTimePicker1;
         private System.Windows.Forms.Button button1;
+        private Guna.UI.WinForms.GunaDataGridView gunaDataGridViewPatients;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label2;
     }
 }
--- TrustWell Hospital/appointment_doc.cs
+++ TrustWell Hospital/appointment_doc.cs
@@ -1,5 +1,6 @@
 ﻿using MySql.Data.MySqlClient;
 using System;
+using System.Collections.Generic;
 using System.Data;
 using System.Windows.Forms;
 using WindowsFormsApp1;
@@ -8,33 +9,63 @@ namespace TrustWell_Hospital
 {
     public partial class appointment_doc : Form
     {
-        public appointment_doc(int DocID)
+        private int doctorid;
+        private string doctorName;
+        private string special;
+        private int fees;
+        public appointment_doc(int DocID, string DOCNAME ,string speci,int fee)
         {
             InitializeComponent();
+            doctorid = DocID;
+            doctorName = DOCNAME;
+            special = speci;
+            fees = fee;
+            LoadDocDetails();
+            LoadPatients();
+
         }
 
-        //private void LoadPatientDeatails()
-        //{
-        //    string query = "SELECT * FROM Patients WHERE DFGDFG = @id";
-        //    MySqlParameter[] parameters =
-        //    {
-        //        new MySqlParameter("@id" , DFGDFG)
-        //    };
+        private void LoadDocDetails()
+        {
+            label7.Text = $"Doctor ID : {doctorid}";
+            label4.Text = $"Doctor Name : {doctorName}";
+            label2.Text = $"Specialization : {special}";
+        }
 
-        //    DataTable dt = Database.ExecuteQuery(query, parameters);
 
-        //    if(dt.Rows.Count > 0)
-        //    {
-        //        DataRow row = dt.Rows[0];
-        //        //now we can add data that are retrived from database :)
-        //    }
-        //}
+        private void LoadPatients(string name = "", string mobile = "")
+        {
+            string query = "SELECT PatientID, PatientName, patientNIC, ContactNumber FROM Patients WHERE 1=1";
+
+            var parameters = new List<MySqlParameter>();
+
+
+            DataTable dt = Database.ExecuteQuery(query, parameters.ToArray());
+
+            gunaDataGridViewPatients.DataSource = dt;
+
+            if (!gunaDataGridViewPatients.Columns.Contains("View"))
+            {
+                DataGridViewButtonColumn viewBtn = new DataGridViewButtonColumn();
+                viewBtn.HeaderText = "Action";
+                viewBtn.Text = "View";
+                viewBtn.UseColumnTextForButtonValue = true;
+                viewBtn.Name = "View";
+                gunaDataGridViewPatients.Columns.Add(viewBtn);
+            }
+        }
+
         private void dateTimePicker1_ValueChanged(object sender, EventArgs e)
         {
 
         }
 
         private void appointment_doc_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
         {
 
         }
