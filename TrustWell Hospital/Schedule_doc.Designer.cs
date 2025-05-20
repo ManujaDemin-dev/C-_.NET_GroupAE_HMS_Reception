d8a69434715ba93c8d997113d9f1d9cc1b7fbc6d
Manuja Demin <mmdwijesinghe@students.nsbm.ac.lk>
2025-05-20 16:41:51 +0530

    DOCTORS COMPLETED [::missing print function]

--- TrustWell Hospital/Schedule_doc.Designer.cs
+++ TrustWell Hospital/Schedule_doc.Designer.cs
@@ -31,17 +31,31 @@
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label8 = new System.Windows.Forms.Label();
             this.label3 = new System.Windows.Forms.Label();
             this.label4 = new System.Windows.Forms.Label();
             this.label5 = new System.Windows.Forms.Label();
             this.label6 = new System.Windows.Forms.Label();
             this.label7 = new System.Windows.Forms.Label();
-            this.panel2 = new System.Windows.Forms.Panel();
-            this.label8 = new System.Windows.Forms.Label();
             this.label9 = new System.Windows.Forms.Label();
             this.label10 = new System.Windows.Forms.Label();
+            this.panel3 = new System.Windows.Forms.Panel();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label18 = new System.Windows.Forms.Label();
+            this.label17 = new System.Windows.Forms.Label();
+            this.label16 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label15 = new System.Windows.Forms.Label();
+            this.label14 = new System.Windows.Forms.Label();
+            this.label13 = new System.Windows.Forms.Label();
+            this.label12 = new System.Windows.Forms.Label();
+            this.label11 = new System.Windows.Forms.Label();
             this.panel1.SuspendLayout();
             this.panel2.SuspendLayout();
+            this.panel3.SuspendLayout();
             this.SuspendLayout();
             // 
             // label1
@@ -75,55 +89,6 @@
             this.panel1.Size = new System.Drawing.Size(1444, 100);
             this.panel1.TabIndex = 2;
             // 
-            // label3
-            // 
-            this.label3.AutoSize = true;
-            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.Location = new System.Drawing.Point(36, 181);
-            this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(256, 31);
-            this.label3.TabIndex = 3;
-            this.label3.Text = "Doctor Name : {name}";
-            // 
-            // label4
-            // 
-            this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label4.Location = new System.Drawing.Point(36, 228);
-            this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(259, 31);
-            this.label4.TabIndex = 4;
-            this.label4.Text = "Doctor special : {spec}";
-            // 
-            // label5
-            // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(1032, 138);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(137, 25);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "Patient id : {id}";
-            // 
-            // label6
-            // 
-            this.label6.AutoSize = true;
-            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label6.Location = new System.Drawing.Point(1032, 181);
-            this.label6.Name = "label6";
-            this.label6.Size = new System.Drawing.Size(0, 31);
-            this.label6.TabIndex = 6;
-            // 
-            // label7
-            // 
-            this.label7.AutoSize = true;
-            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label7.Location = new System.Drawing.Point(1032, 181);
-            this.label7.Name = "label7";
-            this.label7.Size = new System.Drawing.Size(236, 25);
-            this.label7.TabIndex = 7;
-            this.label7.Text = "Contact patient : {contact}";
-            // 
             // panel2
             // 
             this.panel2.BackColor = System.Drawing.SystemColors.ControlDark;
@@ -144,6 +109,55 @@
             this.label8.TabIndex = 1;
             this.label8.Text = "Confirm Appointment";
             // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(36, 181);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(205, 25);
+            this.label3.TabIndex = 3;
+            this.label3.Text = "Doctor Name : {name}";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(36, 228);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(207, 25);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Doctor special : {spec}";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(1032, 138);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(137, 25);
+            this.label5.TabIndex = 5;
+            this.label5.Text = "Patient id : {id}";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(1032, 181);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(0, 25);
+            this.label6.TabIndex = 6;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(1032, 181);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(236, 25);
+            this.label7.TabIndex = 7;
+            this.label7.Text = "Contact patient : {contact}";
+            // 
             // label9
             // 
             this.label9.AutoSize = true;
@@ -160,14 +174,151 @@
             this.label10.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label10.Location = new System.Drawing.Point(36, 324);
             this.label10.Name = "label10";
-            this.label10.Size = new System.Drawing.Size(0, 31);
+            this.label10.Size = new System.Drawing.Size(0, 25);
             this.label10.TabIndex = 9;
+            // 
+            // panel3
+            // 
+            this.panel3.BackColor = System.Drawing.SystemColors.GradientActiveCaption;
+            this.panel3.Controls.Add(this.button3);
+            this.panel3.Controls.Add(this.label18);
+            this.panel3.Controls.Add(this.label17);
+            this.panel3.Controls.Add(this.label16);
+            this.panel3.Controls.Add(this.dateTimePicker1);
+            this.panel3.Controls.Add(this.button2);
+            this.panel3.Controls.Add(this.button1);
+            this.panel3.Controls.Add(this.label15);
+            this.panel3.Controls.Add(this.label14);
+            this.panel3.Controls.Add(this.label13);
+            this.panel3.Controls.Add(this.label12);
+            this.panel3.Controls.Add(this.label11);
+            this.panel3.Location = new System.Drawing.Point(-26, 293);
+            this.panel3.Name = "panel3";
+            this.panel3.Size = new System.Drawing.Size(1477, 619);
+            this.panel3.TabIndex = 10;
+            this.panel3.Paint += new System.Windows.Forms.PaintEventHandler(this.panel3_Paint);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(527, 102);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 21;
+            this.button3.Text = "Confirm";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // label18
+            // 
+            this.label18.AutoSize = true;
+            this.label18.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label18.Location = new System.Drawing.Point(635, 396);
+            this.label18.Name = "label18";
+            this.label18.Size = new System.Drawing.Size(224, 36);
+            this.label18.TabIndex = 20;
+            this.label18.Text = "Choosen date : ";
+            // 
+            // label17
+            // 
+            this.label17.AutoSize = true;
+            this.label17.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label17.Location = new System.Drawing.Point(1057, 103);
+            this.label17.Name = "label17";
+            this.label17.Size = new System.Drawing.Size(224, 36);
+            this.label17.TabIndex = 19;
+            this.label17.Text = "Choosen date : ";
+            // 
+            // label16
+            // 
+            this.label16.AutoSize = true;
+            this.label16.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label16.Location = new System.Drawing.Point(842, 103);
+            this.label16.Name = "label16";
+            this.label16.Size = new System.Drawing.Size(224, 36);
+            this.label16.TabIndex = 18;
+            this.label16.Text = "Choosen date : ";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(321, 103);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 22);
+            this.dateTimePicker1.TabIndex = 17;
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(275, 411);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(178, 61);
+            this.button2.TabIndex = 16;
+            this.button2.Text = "button2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(52, 411);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(178, 61);
+            this.button1.TabIndex = 15;
+            this.button1.Text = "Check_Availability";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label15
+            // 
+            this.label15.AutoSize = true;
+            this.label15.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label15.Location = new System.Drawing.Point(47, 306);
+            this.label15.Name = "label15";
+            this.label15.Size = new System.Drawing.Size(0, 25);
+            this.label15.TabIndex = 14;
+            // 
+            // label14
+            // 
+            this.label14.AutoSize = true;
+            this.label14.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label14.Location = new System.Drawing.Point(47, 352);
+            this.label14.Name = "label14";
+            this.label14.Size = new System.Drawing.Size(0, 25);
+            this.label14.TabIndex = 13;
+            // 
+            // label13
+            // 
+            this.label13.AutoSize = true;
+            this.label13.Font = new System.Drawing.Font("Microsoft Sans Serif", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label13.Location = new System.Drawing.Point(62, 147);
+            this.label13.Name = "label13";
+            this.label13.Size = new System.Drawing.Size(440, 36);
+            this.label13.TabIndex = 12;
+            this.label13.Text = "Doctors Avalability days  :{days}";
+            // 
+            // label12
+            // 
+            this.label12.AutoSize = true;
+            this.label12.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label12.Location = new System.Drawing.Point(62, 100);
+            this.label12.Name = "label12";
+            this.label12.Size = new System.Drawing.Size(253, 25);
+            this.label12.TabIndex = 11;
+            this.label12.Text = "Choose Appointment Date :";
+            // 
+            // label11
+            // 
+            this.label11.AutoSize = true;
+            this.label11.Font = new System.Drawing.Font("Microsoft Sans Serif", 16.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label11.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(0)))), ((int)(((byte)(0)))));
+            this.label11.Location = new System.Drawing.Point(546, 31);
+            this.label11.Name = "label11";
+            this.label11.Size = new System.Drawing.Size(372, 32);
+            this.label11.TabIndex = 11;
+            this.label11.Text = "Confirm Appointment Date";
             // 
             // Schedule_doc
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1444, 818);
+            this.Controls.Add(this.panel3);
             this.Controls.Add(this.label10);
             this.Controls.Add(this.label9);
             this.Controls.Add(this.label7);
@@ -184,6 +335,8 @@
             this.panel1.PerformLayout();
             this.panel2.ResumeLayout(false);
             this.panel2.PerformLayout();
+            this.panel3.ResumeLayout(false);
+            this.panel3.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -203,5 +356,18 @@
         private System.Windows.Forms.Label label7;
         private System.Windows.Forms.Label label9;
         private System.Windows.Forms.Label label10;
+        private System.Windows.Forms.Panel panel3;
+        private System.Windows.Forms.Label label11;
+        private System.Windows.Forms.Label label15;
+        private System.Windows.Forms.Label label14;
+        private System.Windows.Forms.Label label13;
+        private System.Windows.Forms.Label label12;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label17;
+        private System.Windows.Forms.Label label16;
+        private System.Windows.Forms.Label label18;
+        private System.Windows.Forms.Button button3;
     }
 }
--- TrustWell Hospital/Schedule_doc.cs
+++ TrustWell Hospital/Schedule_doc.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MySql.Data.MySqlClient;
+using System;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Data;
@@ -7,6 +8,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using WindowsFormsApp1;
 
 namespace TrustWell_Hospital
 {
@@ -29,6 +31,12 @@ namespace TrustWell_Hospital
             PatientName = patientname;
             RefNo = refno;
             PatientMobile = patientmobile;
+            HeaderLoad();
+            AvalabilityData();
+        }
+
+        private void HeaderLoad()
+        {
             label1.Text = $"Doctor ID : {Docid}";
             label3.Text = $"Doctor Name : {DoctorName}";
             label4.Text = $"Doctor Specialization : {Special}";
@@ -39,8 +47,78 @@ namespace TrustWell_Hospital
             //label10.Text = $"hELLOW DOC ID : {RefNo}";
         }
 
+        List<string> docDay = new List<string>();
+
+        private void AvalabilityData()
+        {
+            string query = "SELECT available_day, start_time, end_time FROM Doc_availability WHERE DoctorID = @DOCID";
+            MySqlParameter[] parameters = {
+            new MySqlParameter("@DOCID", Docid)
+        };
+
+            DataTable dt = Database.ExecuteQuery(query, parameters);
+
+            if (dt.Rows.Count > 0)
+            {
+                StringBuilder sb = new StringBuilder();
+
+                foreach (DataRow row in dt.Rows)
+                {
+                    string day = row["available_day"].ToString();
+                    string startTime = row["start_time"].ToString();
+                    string endTime = row["end_time"].ToString();
+                    //DataRow row = dt.Rows[0];
+
+                    sb.AppendLine($"- {day}: {startTime} to {endTime}");
+                    docDay.Add(day);
+                }
+                
+                label13.Text = $"Doctor Avalability Days: = {sb.ToString()}";
+                label18.Text = $"{docDay}";
+
+            }
+            else
+            {
+                label13.Text = "No Availability data found for this doctor :(";
+
+            }
+                
+        }
+
+
+
+
         private void Schedule_doc_Load(object sender, EventArgs e)
         {
+
+        }
+
+        private void panel3_Paint(object sender, PaintEventArgs e)
+        {
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            label17.Text = dateTimePicker1.Value.DayOfWeek.ToString();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string selectedDay = label17.Text;
+
+            if (docDay.Contains(selectedDay))
+            {
+                MessageBox.Show("Doctor is available on this day :)");
+                label18.Text = "Doctor is available on this day :)"; 
+            }
+            else
+            {
+
+                MessageBox.Show("Doctor is not available on this selected day.");
+                label18.Text = "Doctor is not available ";
+
+            }
 
         }
     }
