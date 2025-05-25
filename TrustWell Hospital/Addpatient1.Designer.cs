eb900a8a9c15660bdfaef93299d80907463b3440
Sithija_Imesh <156312972+Sithijaimesh@users.noreply.github.com>
2025-05-25 21:22:27 +0530

    Update the design Addpatient1

--- TrustWell Hospital/Addpatient1.Designer.cs
+++ TrustWell Hospital/Addpatient1.Designer.cs
@@ -29,120 +29,168 @@
         private void InitializeComponent()
         {
             this.panel1 = new System.Windows.Forms.Panel();
-            this.button1 = new System.Windows.Forms.Button();
-            this.textBox2 = new System.Windows.Forms.TextBox();
-            this.textBox1 = new System.Windows.Forms.TextBox();
             this.label4 = new System.Windows.Forms.Label();
-            this.label3 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new Guna.UI.WinForms.GunaTextBox();
+            this.textBox2 = new Guna.UI.WinForms.GunaTextBox();
+            this.button1 = new Guna.UI.WinForms.GunaButton();
+            this.cuiPanel2 = new CuoreUI.Controls.cuiPanel();
             this.panel1.SuspendLayout();
+            this.cuiPanel2.SuspendLayout();
             this.SuspendLayout();
             // 
             // panel1
             // 
-            this.panel1.BackColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.panel1.Controls.Add(this.button1);
-            this.panel1.Controls.Add(this.textBox2);
-            this.panel1.Controls.Add(this.textBox1);
-            this.panel1.Controls.Add(this.label4);
-            this.panel1.Controls.Add(this.label3);
-            this.panel1.Controls.Add(this.label2);
-            this.panel1.Controls.Add(this.label1);
-            this.panel1.Location = new System.Drawing.Point(25, 19);
-            this.panel1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
+            this.panel1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(250)))), ((int)(((byte)(251)))));
+            this.panel1.Controls.Add(this.cuiPanel2);
+            this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(1660, 769);
+            this.panel1.Size = new System.Drawing.Size(1589, 802);
             this.panel1.TabIndex = 0;
             this.panel1.Paint += new System.Windows.Forms.PaintEventHandler(this.panel1_Paint);
-            // 
-            // button1
-            // 
-            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(25)))), ((int)(((byte)(118)))), ((int)(((byte)(210)))));
-            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.button1.ForeColor = System.Drawing.SystemColors.Control;
-            this.button1.Location = new System.Drawing.Point(704, 408);
-            this.button1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(162, 56);
-            this.button1.TabIndex = 6;
-            this.button1.Text = "Check";
-            this.button1.UseVisualStyleBackColor = false;
-            // 
-            // textBox2
-            // 
-            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox2.Location = new System.Drawing.Point(274, 280);
-            this.textBox2.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(592, 39);
-            this.textBox2.TabIndex = 5;
-            // 
-            // textBox1
-            // 
-            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.textBox1.Location = new System.Drawing.Point(274, 168);
-            this.textBox1.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
-            this.textBox1.Name = "textBox1";
-            this.textBox1.Size = new System.Drawing.Size(592, 39);
-            this.textBox1.TabIndex = 4;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Font = new System.Drawing.Font("Segoe UI", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.label4.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
-            this.label4.Location = new System.Drawing.Point(97, 87);
+            this.label4.Location = new System.Drawing.Point(107, 53);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(743, 29);
+            this.label4.Size = new System.Drawing.Size(638, 31);
             this.label4.TabIndex = 3;
-            this.label4.Text = "Enter Patient deatails for check the patient is already registerd.";
+            this.label4.Text = "Enter Patient details for check  patient is already registerd.";
+            this.label4.Click += new System.EventHandler(this.label4_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
+            this.label2.Location = new System.Drawing.Point(164, 226);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(119, 28);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Patient NIC:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Segoe UI Semibold", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
+            this.label1.Location = new System.Drawing.Point(157, 150);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(140, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Patient Name:";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
             // label3
             // 
             this.label3.AutoSize = true;
             this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label3.ForeColor = System.Drawing.SystemColors.GradientInactiveCaption;
-            this.label3.Location = new System.Drawing.Point(103, 399);
+            this.label3.ForeColor = System.Drawing.Color.Red;
+            this.label3.Location = new System.Drawing.Point(157, 383);
             this.label3.Name = "label3";
-            this.label3.Size = new System.Drawing.Size(37, 29);
+            this.label3.Size = new System.Drawing.Size(0, 25);
             this.label3.TabIndex = 2;
-            this.label3.Text = "kk";
             // 
-            // label2
+            // textBox1
             // 
-            this.label2.AutoSize = true;
-            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
-            this.label2.Location = new System.Drawing.Point(103, 288);
-            this.label2.Name = "label2";
-            this.label2.Size = new System.Drawing.Size(130, 26);
-            this.label2.TabIndex = 1;
-            this.label2.Text = "Patient NIC:";
+            this.textBox1.BackColor = System.Drawing.Color.Transparent;
+            this.textBox1.BaseColor = System.Drawing.Color.White;
+            this.textBox1.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
+            this.textBox1.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox1.FocusedBaseColor = System.Drawing.Color.White;
+            this.textBox1.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.textBox1.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox1.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(372, 150);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.PasswordChar = '\0';
+            this.textBox1.Radius = 7;
+            this.textBox1.SelectedText = "";
+            this.textBox1.Size = new System.Drawing.Size(342, 40);
+            this.textBox1.TabIndex = 7;
             // 
-            // label1
+            // textBox2
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(97)))), ((int)(((byte)(97)))), ((int)(((byte)(97)))));
-            this.label1.Location = new System.Drawing.Point(103, 176);
-            this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(151, 26);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Patient Name:";
-            this.label1.Click += new System.EventHandler(this.label1_Click);
+            this.textBox2.BackColor = System.Drawing.Color.Transparent;
+            this.textBox2.BaseColor = System.Drawing.Color.White;
+            this.textBox2.BorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
+            this.textBox2.Cursor = System.Windows.Forms.Cursors.IBeam;
+            this.textBox2.FocusedBaseColor = System.Drawing.Color.White;
+            this.textBox2.FocusedBorderColor = System.Drawing.Color.FromArgb(((int)(((byte)(100)))), ((int)(((byte)(88)))), ((int)(((byte)(255)))));
+            this.textBox2.FocusedForeColor = System.Drawing.SystemColors.ControlText;
+            this.textBox2.Font = new System.Drawing.Font("Segoe UI", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox2.Location = new System.Drawing.Point(373, 214);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.PasswordChar = '\0';
+            this.textBox2.Radius = 7;
+            this.textBox2.SelectedText = "";
+            this.textBox2.Size = new System.Drawing.Size(338, 42);
+            this.textBox2.TabIndex = 7;
+            this.textBox2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // button1
+            // 
+            this.button1.AnimationHoverSpeed = 0.07F;
+            this.button1.AnimationSpeed = 0.03F;
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.BaseColor = System.Drawing.Color.FromArgb(((int)(((byte)(59)))), ((int)(((byte)(130)))), ((int)(((byte)(246)))));
+            this.button1.BorderColor = System.Drawing.Color.Transparent;
+            this.button1.BorderSize = 2;
+            this.button1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.button1.FocusedColor = System.Drawing.Color.Empty;
+            this.button1.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Image = null;
+            this.button1.ImageSize = new System.Drawing.Size(20, 20);
+            this.button1.Location = new System.Drawing.Point(698, 315);
+            this.button1.Name = "button1";
+            this.button1.OnHoverBaseColor = System.Drawing.Color.Blue;
+            this.button1.OnHoverBorderColor = System.Drawing.Color.Transparent;
+            this.button1.OnHoverForeColor = System.Drawing.Color.White;
+            this.button1.OnHoverImage = null;
+            this.button1.OnPressedColor = System.Drawing.Color.Black;
+            this.button1.Radius = 8;
+            this.button1.Size = new System.Drawing.Size(160, 42);
+            this.button1.TabIndex = 8;
+            this.button1.Text = "Search";
+            this.button1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // cuiPanel2
+            // 
+            this.cuiPanel2.Controls.Add(this.textBox1);
+            this.cuiPanel2.Controls.Add(this.button1);
+            this.cuiPanel2.Controls.Add(this.label1);
+            this.cuiPanel2.Controls.Add(this.textBox2);
+            this.cuiPanel2.Controls.Add(this.label2);
+            this.cuiPanel2.Controls.Add(this.label3);
+            this.cuiPanel2.Controls.Add(this.label4);
+            this.cuiPanel2.Font = new System.Drawing.Font("Segoe UI Semibold", 10.2F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiPanel2.Location = new System.Drawing.Point(238, 106);
+            this.cuiPanel2.Name = "cuiPanel2";
+            this.cuiPanel2.OutlineThickness = 2F;
+            this.cuiPanel2.PanelColor = System.Drawing.Color.FromArgb(((int)(((byte)(249)))), ((int)(((byte)(250)))), ((int)(((byte)(251)))));
+            this.cuiPanel2.PanelOutlineColor = System.Drawing.Color.Gray;
+            this.cuiPanel2.Rounding = new System.Windows.Forms.Padding(10);
+            this.cuiPanel2.Size = new System.Drawing.Size(973, 494);
+            this.cuiPanel2.TabIndex = 10;
             // 
             // Addpatient1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.Controls.Add(this.panel1);
-            this.Margin = new System.Windows.Forms.Padding(3, 4, 3, 4);
             this.Name = "Addpatient1";
-            this.Size = new System.Drawing.Size(1788, 1006);
+            this.Size = new System.Drawing.Size(1589, 805);
             this.Load += new System.EventHandler(this.Addpatient1_Load);
             this.panel1.ResumeLayout(false);
-            this.panel1.PerformLayout();
+            this.cuiPanel2.ResumeLayout(false);
+            this.cuiPanel2.PerformLayout();
             this.ResumeLayout(false);
 
         }
@@ -150,12 +198,13 @@
         #endregion
 
         private System.Windows.Forms.Panel panel1;
-        private System.Windows.Forms.Label label3;
         private System.Windows.Forms.Label label2;
         private System.Windows.Forms.Label label1;
-        private System.Windows.Forms.TextBox textBox2;
-        private System.Windows.Forms.TextBox textBox1;
         private System.Windows.Forms.Label label4;
-        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label3;
+        private Guna.UI.WinForms.GunaTextBox textBox2;
+        private Guna.UI.WinForms.GunaTextBox textBox1;
+        private Guna.UI.WinForms.GunaButton button1;
+        private CuoreUI.Controls.cuiPanel cuiPanel2;
     }
 }
