4ee2c37fc4497203296288db1b114890a4c07f69
hesrodrigo <hesararodrigo987@gmail.com>
2025-05-27 16:12:29 +0530

    updateInfomation
    updated the patient info

--- TrustWell Hospital/PatientMiniPage.Designer.cs
+++ TrustWell Hospital/PatientMiniPage.Designer.cs
@@ -31,12 +31,19 @@
             this.lblName = new System.Windows.Forms.Label();
             this.lblAddress = new System.Windows.Forms.Label();
             this.lblEmail = new System.Windows.Forms.Label();
-            this.lblPhone = new System.Windows.Forms.Label();
+            this.lblContact = new System.Windows.Forms.Label();
             this.lblDOB = new System.Windows.Forms.Label();
-            this.label5 = new System.Windows.Forms.Label();
+            this.lblGender = new System.Windows.Forms.Label();
             this.lblNIC = new System.Windows.Forms.Label();
             this.gunaPanel1 = new Guna.UI.WinForms.GunaPanel();
             this.label1 = new System.Windows.Forms.Label();
+            this.cuiButton1 = new CuoreUI.Controls.cuiButton();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
             this.gunaPanel1.SuspendLayout();
             this.SuspendLayout();
             // 
@@ -44,7 +51,7 @@
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblName.Location = new System.Drawing.Point(94, 131);
+            this.lblName.Location = new System.Drawing.Point(166, 135);
             this.lblName.Name = "lblName";
             this.lblName.Size = new System.Drawing.Size(59, 25);
             this.lblName.TabIndex = 0;
@@ -54,7 +61,7 @@
             // 
             this.lblAddress.AutoSize = true;
             this.lblAddress.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblAddress.Location = new System.Drawing.Point(94, 427);
+            this.lblAddress.Location = new System.Drawing.Point(166, 410);
             this.lblAddress.Name = "lblAddress";
             this.lblAddress.Size = new System.Drawing.Size(59, 25);
             this.lblAddress.TabIndex = 1;
@@ -64,47 +71,47 @@
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblEmail.Location = new System.Drawing.Point(94, 379);
+            this.lblEmail.Location = new System.Drawing.Point(166, 361);
             this.lblEmail.Name = "lblEmail";
             this.lblEmail.Size = new System.Drawing.Size(59, 25);
             this.lblEmail.TabIndex = 2;
             this.lblEmail.Text = "label1";
             // 
-            // lblPhone
+            // lblContact
             // 
-            this.lblPhone.AutoSize = true;
-            this.lblPhone.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblPhone.Location = new System.Drawing.Point(94, 332);
-            this.lblPhone.Name = "lblPhone";
-            this.lblPhone.Size = new System.Drawing.Size(59, 25);
-            this.lblPhone.TabIndex = 3;
-            this.lblPhone.Text = "label1";
+            this.lblContact.AutoSize = true;
+            this.lblContact.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblContact.Location = new System.Drawing.Point(166, 315);
+            this.lblContact.Name = "lblContact";
+            this.lblContact.Size = new System.Drawing.Size(59, 25);
+            this.lblContact.TabIndex = 3;
+            this.lblContact.Text = "label1";
             // 
             // lblDOB
             // 
             this.lblDOB.AutoSize = true;
             this.lblDOB.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblDOB.Location = new System.Drawing.Point(94, 280);
+            this.lblDOB.Location = new System.Drawing.Point(166, 276);
             this.lblDOB.Name = "lblDOB";
             this.lblDOB.Size = new System.Drawing.Size(59, 25);
             this.lblDOB.TabIndex = 4;
             this.lblDOB.Text = "label1";
             // 
-            // label5
+            // lblGender
             // 
-            this.label5.AutoSize = true;
-            this.label5.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label5.Location = new System.Drawing.Point(93, 229);
-            this.label5.Name = "label5";
-            this.label5.Size = new System.Drawing.Size(59, 25);
-            this.label5.TabIndex = 5;
-            this.label5.Text = "label1";
+            this.lblGender.AutoSize = true;
+            this.lblGender.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblGender.Location = new System.Drawing.Point(166, 229);
+            this.lblGender.Name = "lblGender";
+            this.lblGender.Size = new System.Drawing.Size(59, 25);
+            this.lblGender.TabIndex = 5;
+            this.lblGender.Text = "label1";
             // 
             // lblNIC
             // 
             this.lblNIC.AutoSize = true;
             this.lblNIC.Font = new System.Drawing.Font("Segoe UI Semibold", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblNIC.Location = new System.Drawing.Point(94, 178);
+            this.lblNIC.Location = new System.Drawing.Point(166, 180);
             this.lblNIC.Name = "lblNIC";
             this.lblNIC.Size = new System.Drawing.Size(59, 25);
             this.lblNIC.TabIndex = 6;
@@ -116,7 +123,7 @@
             this.gunaPanel1.Controls.Add(this.label1);
             this.gunaPanel1.Location = new System.Drawing.Point(12, 8);
             this.gunaPanel1.Name = "gunaPanel1";
-            this.gunaPanel1.Size = new System.Drawing.Size(833, 74);
+            this.gunaPanel1.Size = new System.Drawing.Size(1160, 74);
             this.gunaPanel1.TabIndex = 7;
             // 
             // label1
@@ -132,16 +139,116 @@
             this.label1.Text = "Patient Information";
             this.label1.Click += new System.EventHandler(this.label1_Click);
             // 
+            // cuiButton1
+            // 
+            this.cuiButton1.CheckButton = false;
+            this.cuiButton1.Checked = false;
+            this.cuiButton1.CheckedBackground = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton1.CheckedForeColor = System.Drawing.Color.White;
+            this.cuiButton1.CheckedImageTint = System.Drawing.Color.White;
+            this.cuiButton1.CheckedOutline = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(106)))), ((int)(((byte)(0)))));
+            this.cuiButton1.Content = "Edit ";
+            this.cuiButton1.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.cuiButton1.DialogResult = System.Windows.Forms.DialogResult.None;
+            this.cuiButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.cuiButton1.ForeColor = System.Drawing.Color.Black;
+            this.cuiButton1.HoverBackground = System.Drawing.Color.White;
+            this.cuiButton1.HoveredImageTint = System.Drawing.Color.White;
+            this.cuiButton1.HoverForeColor = System.Drawing.Color.Black;
+            this.cuiButton1.HoverOutline = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.cuiButton1.Image = null;
+            this.cuiButton1.ImageAutoCenter = true;
+            this.cuiButton1.ImageExpand = new System.Drawing.Point(0, 0);
+            this.cuiButton1.ImageOffset = new System.Drawing.Point(0, 0);
+            this.cuiButton1.Location = new System.Drawing.Point(717, 436);
+            this.cuiButton1.Name = "cuiButton1";
+            this.cuiButton1.NormalBackground = System.Drawing.Color.LightSkyBlue;
+            this.cuiButton1.NormalForeColor = System.Drawing.Color.Black;
+            this.cuiButton1.NormalImageTint = System.Drawing.Color.White;
+            this.cuiButton1.NormalOutline = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.cuiButton1.OutlineThickness = 1F;
+            this.cuiButton1.PressedBackground = System.Drawing.Color.WhiteSmoke;
+            this.cuiButton1.PressedForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(32)))), ((int)(((byte)(32)))), ((int)(((byte)(32)))));
+            this.cuiButton1.PressedImageTint = System.Drawing.Color.White;
+            this.cuiButton1.PressedOutline = System.Drawing.Color.FromArgb(((int)(((byte)(64)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))), ((int)(((byte)(128)))));
+            this.cuiButton1.Rounding = new System.Windows.Forms.Padding(8);
+            this.cuiButton1.Size = new System.Drawing.Size(153, 45);
+            this.cuiButton1.TabIndex = 16;
+            this.cuiButton1.TextAlignment = System.Drawing.StringAlignment.Center;
+            this.cuiButton1.TextOffset = new System.Drawing.Point(0, 0);
+            this.cuiButton1.Click += new System.EventHandler(this.cuiButton1_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(714, 142);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(425, 29);
+            this.label2.TabIndex = 17;
+            this.label2.Text = "label2";
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(712, 180);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(293, 29);
+            this.label3.TabIndex = 18;
+            this.label3.Text = "label3";
+            // 
+            // label4
+            // 
+            this.label4.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(714, 225);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(293, 29);
+            this.label4.TabIndex = 19;
+            this.label4.Text = "label4";
+            // 
+            // label5
+            // 
+            this.label5.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(714, 276);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(293, 29);
+            this.label5.TabIndex = 20;
+            this.label5.Text = "label5";
+            // 
+            // label6
+            // 
+            this.label6.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label6.Location = new System.Drawing.Point(714, 324);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(293, 29);
+            this.label6.TabIndex = 21;
+            this.label6.Text = "label6";
+            // 
+            // label7
+            // 
+            this.label7.Font = new System.Drawing.Font("Segoe UI Symbol", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label7.Location = new System.Drawing.Point(714, 361);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(447, 59);
+            this.label7.TabIndex = 22;
+            this.label7.Text = "label7";
+            // 
             // PatientMiniPage
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 493);
+            this.ClientSize = new System.Drawing.Size(1173, 658);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.cuiButton1);
             this.Controls.Add(this.gunaPanel1);
             this.Controls.Add(this.lblNIC);
-            this.Controls.Add(this.label5);
+            this.Controls.Add(this.lblGender);
             this.Controls.Add(this.lblDOB);
-            this.Controls.Add(this.lblPhone);
+            this.Controls.Add(this.lblContact);
             this.Controls.Add(this.lblEmail);
             this.Controls.Add(this.lblAddress);
             this.Controls.Add(this.lblName);
@@ -164,11 +271,18 @@
         private System.Windows.Forms.Label lblName;
         private System.Windows.Forms.Label lblAddress;
         private System.Windows.Forms.Label lblEmail;
-        private System.Windows.Forms.Label lblPhone;
+        private System.Windows.Forms.Label lblContact;
         private System.Windows.Forms.Label lblDOB;
-        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label lblGender;
         private System.Windows.Forms.Label lblNIC;
         private Guna.UI.WinForms.GunaPanel gunaPanel1;
         private System.Windows.Forms.Label label1;
+        private CuoreUI.Controls.cuiButton cuiButton1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
     }
 }
---
+++ TrustWell Hospital/UpdateInformation.cs
@@ -0,0 +1,98 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using WindowsFormsApp1;
+
+namespace TrustWell_Hospital
+{
+    public partial class UpdateInformation : Form
+    {
+        private int patientID;
+        public UpdateInformation(int id)
+        {
+            InitializeComponent();
+            patientID = id;
+        }
+
+        private void UpdateInformation_Load(object sender, EventArgs e)
+        {
+            {
+                MySqlParameter[] parameters = {
+                new MySqlParameter("@id", patientID)
+            };
+
+                // Load Patient Information
+                string query = "SELECT * FROM Patients WHERE PatientID = @id";
+
+                try
+                {
+                    DataTable dt = Database.ExecuteQuery(query, parameters);
+
+                    if (dt.Rows.Count > 0)
+                    {
+                        DataRow row = dt.Rows[0];
+
+                        pName.Text = row["PatientName"].ToString();
+                        NIC.Text = row["patientNIC"].ToString();
+                        pGender.Text = row["Gender"].ToString();
+
+                        phone.Text = row["ContactNumber"].ToString();
+                        email.Text = row["Email"].ToString();
+                        address.Text = row["Address"].ToString();
+                    }
+
+                }
+                catch (Exception ex)
+                {
+                    MessageBox.Show("Error! is from :::" + ex.Message);
+                }
+
+                // Load Guardian (Emergency Contact) Information
+
+                try
+                {
+                    string query1 = "SELECT * FROM EmergeencyContacts WHERE PatientID = @id";
+                    MySqlParameter[] parameter1 = {
+                new MySqlParameter("@id", patientID)
+            };
+                    DataTable guardianDt = Database.ExecuteQuery(query1, parameter1);
+
+
+                    if (guardianDt.Rows.Count > 0)
+                    {
+                        DataRow row = guardianDt.Rows[0];
+
+                        gName.Text = row["ContactName"].ToString();
+                        gender.Text = row["Gender"].ToString();
+                        gNIC.Text = row["NIC"].ToString();
+                        relation.Text = row["Relationship"].ToString();
+                        contact.Text = row["ContactNumber"].ToString();
+                        gEmail.Text = row["Email"].ToString();
+
+                    }
+                    else
+                    {
+                        MessageBox.Show("Guardian not found for patient ID = " + patientID);
+                    }
+
+                }
+                catch (Exception eg)
+                {
+                    MessageBox.Show("Error form : " + eg.Message);
+                }
+            }
+
+        }
+
+        private void submit_Click(object sender, EventArgs e)
+        {
+        }
+    }
+}
